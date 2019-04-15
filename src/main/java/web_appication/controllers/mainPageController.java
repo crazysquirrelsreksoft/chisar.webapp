42ac072b03dc7f09b33b4a14273717c3741606d0
crazysquirrelsreksoft <crazysquirrels@mail.ru>
2019-04-15 21:02:14 +0300

    Added BootStrap main page

--- src/main/java/web_appication/controllers/mainPageController.java
+++ src/main/java/web_appication/controllers/mainPageController.java
@@ -1,15 +1,26 @@
 package web_appication.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import web_appication.services.TripListService;
 
 @Controller
 public class mainPageController {
 
-    @GetMapping("/homepage")
-    public String getMainPage(Model model){
-        return "mainPage";
+    @Autowired
+    private TripListService tripListService;
+
+//    @GetMapping("/homepage")
+//    public String getMainPage(Model model){
+//        return "mainPage";
+//    }
+
+    @GetMapping("/mainPage")
+    public  String getMainPage(Model model){
+        model.addAttribute("tripItemList", tripListService.getTripList());
+        return "BootStrapPage";
     }
 
 }
